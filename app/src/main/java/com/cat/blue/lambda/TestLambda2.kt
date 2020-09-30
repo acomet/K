b9ae5e34abc46849c014abd48fae1fe44624bb74
relmin <relmin@163.com>
2020-09-30 23:09:12 +0800

    commit from mac

--- app/src/main/java/com/cat/blue/lambda/TestLambda2.kt
+++ app/src/main/java/com/cat/blue/lambda/TestLambda2.kt
@@ -12,41 +12,79 @@ fun main() {
 //    }
 
     // 3
-    alpha1(100) {
-        println(it)
-        return@alpha1 "origin = $it , and result is 'HELLO WORLD'"
+//    alpha1(100, { a -> "hello world" })
+//    alpha1(100) { a -> "hello world" }
+//    alpha1(100) {
+//        println(it)
+//        println(it)
+//        "Hello cat"
+//    }
+//
+//    alpha1(100) {
+//        println(it)
+//        return@alpha1 "origin = $it , and result is 'HELLO WORLD'"
+//    }
+
+    // 4
+//    alpha2(100, 800) { a, b ->
+//        a * b
+//    }
+
+    // 5
+    alpha3(5, 6) { a, b ->
+        println("返回多个结果的另类操作，神奇吧 a=$a, b=$b")
     }
 
-
-    beta(8, 9) { a, b ->
-        println("hello")
-        println("world")
-        println("hi")
-        a * b
-    }
+    // 6
+//    alpha4(8, 10) { a, b ->
+//        a * b
+//    }.apply {
+//        println("--001--$this")
+//    }.apply {
+//        println("--002--$this")
+//    }.also {
+//        println("--003--$it")
+//    }.also {
+//        println("--004--$it")
+//    }
 }
 
-// 方法参数有lambda，lambda无参数，无返回值， 方法整体没有返回值
-fun nothing(x: Int, predicate: () -> Unit) {
-    println("nothing原始x=$x")
-    predicate.invoke()
+// 方法有lambda，lambda有两个参数，有返回值， 方法整体有放回值
+fun alpha4(x: Int, y: Int, predicate: (Int, Int) -> Int): Int {
+    println("alpha4 原始x=$x, y=$y")
+    return predicate(x, y)
 }
 
-// 方法参数有lambda，lambda有参数，无返回值， 方法整体没有返回值
+// 方法有lambda，lambda有两个参数，无返回值， 方法整体无返回值【一般用于返回多个参数，这个就是kotlin牛逼的地方，可以返回多个参数】
+fun alpha3(x: Int, y: Int, predicate: (Int, Int) -> Unit) {
+    println("alpha3 原始x=$x, y=$y")
+    predicate(x * x, y * y)
+}
+
+// 方法有lambda，lambda有两个参数，有返回值，方法整体没有返回值（其实就是同下）
+fun alpha2(x: Int, y: Int, predicate: (Int, Int) -> Int) {
+    println("alpha2 原始x=$x，y=$y")
+    val result = predicate(x, y)
+    println(result)
+}
+
+// 方法参数有lambda，lambda有参数，有返回值， 方法整体没有返回值
+fun alpha1(x: Int, predicate: (Int) -> String) {
+    println("alpha1原始x=$x")
+    val result = predicate.invoke(x + 100 + 200 + 300) // 在这里能拿到lambda的返回值
+    predicate.invoke(x + 100 + 200 + 300)
+}
+
+// 方法参数有lambda，lambda有参数，无返回值， 方法整体没有返回值【一般用于处理回调】
 fun alpha0(x: Int, callback: (Int) -> Unit) {
     println("alpha0原始x=$x")
     val result = x + 100 + 200 + 300
     callback(result)
 }
 
-// 方法参数有lambda，lambda有参数，有返回值， 方法整体没有返回值
-fun alpha1(x: Int, predicate: (Int) -> String) {
-    println("alpha1原始x=$x")
-    val result = x + 100 + 200 + 300
-    predicate.invoke(result)
-}
 
-fun beta(x: Int, y: Int, predicate: (Int, Int) -> Int) {
-    println("beta 原始x=$x")
-    predicate(x, y)
+// 方法参数有lambda，lambda无参数，无返回值， 方法整体没有返回值【很少用】
+fun nothing(x: Int, predicate: () -> Unit) {
+    println("nothing原始x=$x")
+    predicate.invoke()
 }
